85097cf0658d7408d396594d1e7895f9c3f9f921
Taufik Amaryansyah <taufik.amary@gmail.com>
2025-10-11 23:09:26 +0700

    feat: Navigate to home screen on successful login
    Updates the login navigation flow to navigate to the home screen when the `onLoginSuccess` event is triggered. The previous `TODO` has been replaced with a navigation call to `HomeRoute`.

--- app/src/main/java/com/amary/poke/mobile/presentation/login/LoginNav.kt
+++ app/src/main/java/com/amary/poke/mobile/presentation/login/LoginNav.kt
@@ -1,10 +1,9 @@
 package com.amary.poke.mobile.presentation.login
 
-import androidx.compose.runtime.getValue
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import com.amary.poke.mobile.presentation.home.HomeRoute
 import com.amary.poke.mobile.presentation.register.RegisterRoute
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -22,7 +21,7 @@ fun NavGraphBuilder.loginScreen(navController: NavHostController) {
             events = viewModel.events,
             onLogin = viewModel::login,
             onLoginSuccess = {
-                //TODO: navigate to home
+                navController.navigate(HomeRoute)
             },
             onNavigateToRegister = {
                 navController.navigate(RegisterRoute)
